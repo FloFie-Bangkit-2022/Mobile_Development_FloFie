d1d1ac98ad2aa8bce220f47cf1a6f502775a0514
Iqbal-D <111201912093@mhs.dinus.ac.id>
2022-06-13 12:10:40 +0700

    Update some components

--- app/src/main/java/com/capstone/flofie/ui/detail/shop/ShopActivity.kt
+++ app/src/main/java/com/capstone/flofie/ui/detail/shop/ShopActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.capstone.flofie.R
